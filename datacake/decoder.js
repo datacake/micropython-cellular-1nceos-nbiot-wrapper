96698aa79c4ab8fff278931dcfd5a8f480674c0c
Simon Kemper <simon@simonkemper.de>
2022-04-13 19:24:51 +0200

    Extendes Pysense Examples, Added Decoder

---
+++ datacake/decoder.js
@@ -0,0 +1,98 @@
+/*
+    iotcreators Standard Device Template
+    (c) Datacake GmbH
+    Feel free to modify
+*/
+
+// helper function
+function hexToBytes(hex) {
+    for (var bytes = [], c = 0; c < hex.length; c += 2)
+        bytes.push(parseInt(hex.substr(c, 2), 16));
+    return bytes;
+}
+
+// actual decoder
+function Decoder(request) {
+
+    // convert the incoming webhook payload to json object
+    var payload = JSON.parse(request.body);
+
+    // access reports inside webhook from iotcreators
+    var reports = payload.reports;
+
+    // we will need this to store data we want to send to datacake
+    var datacakeFields = [];
+
+    // webhook from iotcreators can contain more than one device
+    for (var i = 0; i < reports.length; i++) {
+
+        var report = reports[i];
+        
+        // extract serial number or IMEI from report
+        // one report = one device
+        var serial = report.serialNumber;
+
+        // extract sensor payload
+        var sensorPayload = report.value;
+
+        // convert byte string into hex-buffer
+        // we need to do this in order to work with byte decoding
+        var bytes = hexToBytes(sensorPayload);
+
+        // extract and convert bytes
+        var temperature = ((bytes[0] << 8) + bytes[1]) / 10.0;
+        var humidity = ((bytes[2] << 8) + bytes[3]) / 10.0;
+        var battery = bytes[4] / 10.0;
+        var pressure = ((bytes[5] << 8) + bytes[6]);
+        var light = ((bytes[7] << 8) + bytes[8]);
+        var lte_seconds = ((bytes[9] << 8) + bytes[10]);
+        
+        // Example for GPS Location
+        var lat = 53.362; // please adapt to your device
+        var lon = 6.394; // please adapt 
+        var location = "(" + lat + "," + lon + ")";
+
+      	// now create an array which we are forwarding to Datacake
+        // this array should contain dictionaries each holding a measurement value
+        datacakeFields.push(
+            {
+                "device": serial, // serial number so IMEI of Device on Datacake
+                "field": "TEMPERATURE", // field identifier on database
+                "value": temperature, // value for field coming out of webhook
+            },
+            {
+                "device": serial,
+                "field": "HUMIDITY",
+                "value": humidity,
+            },
+            {
+                "device": serial,
+                "field": "BATTERY",
+                "value": battery,
+            },
+            {
+                "device": serial,
+                "field": "PRESSURE",
+                "value": pressure
+            },
+            {
+                "device": serial,
+                "field": "LIGHT",
+                "value": light
+            },
+            {
+                "device": serial,
+                "field": "LTE_SECONDS",
+                "value": lte_seconds
+            },
+            {
+                "device": serial,
+                "field": "LOCATION",
+                "value": location,
+            }
+        );
+    }
+		
+    // returning sends the data to Datacake
+    return datacakeFields;
+}
--- examples/pysense_send.py
+++ examples/pysense_send.py
@@ -10,6 +10,8 @@ from lib.pycoproc2 import Pycoproc
 from lib.SI7006A20 import SI7006A20
 from lib.LTR329ALS01 import LTR329ALS01
 from lib.MPL3115A2 import MPL3115A2,PRESSURE
+from machine import WDT, Timer
+import time
 import struct
 import math
 
@@ -18,19 +20,31 @@ IOTCREATORS_UDP_PORT = 15683
 IOTCREATORS_APN = "cdp.iot.t-mobile.nl"
 IOTCREATORS_BAND = 8
 SLEEP_TIME = 300
+WATCHDOG_TIMEOUT = 60000
 DEBUG = True
 
 print("\nStarting Example: pysense_send.py\n")
 
+# Watchdog Timer
+wdt = WDT(timeout=WATCHDOG_TIMEOUT)
+
+# Runtime Stopwatch
+stopwatch = Timer.Chrono()
+stopwatch.start()
+
 # Init Pysense sensors
 py = Pycoproc()
 dht = SI7006A20(py)
+ltr = LTR329ALS01(py)
+mpl = MPL3115A2(py,mode=PRESSURE)
 
 # Read Pysense sensors
 temperature = dht.temperature()
 humidity = dht.humidity()
+pressure = mpl.pressure() / 100.0
+light = ltr.lux()
 battery = py.read_battery_voltage()
-if DEBUG: print("Temp: {}, Hum: {}, Bat: {}\n".format(temperature, humidity, battery))
+if DEBUG: print("Temp: {}, Hum: {}, Pres: {}, Light: {}, Bat: {}\n".format(temperature, humidity, pressure, light, battery))
 
 # Init Wrapper
 message = UDPUplinkMessageWrapper(IOTCREATORS_UDP_IP, IOTCREATORS_UDP_PORT)
@@ -40,18 +54,22 @@ message_payload = bytearray()
 message_payload.extend(struct.pack('>H', math.floor(temperature * 10)))
 message_payload.extend(struct.pack('>H', math.floor(humidity * 10)))
 message_payload.extend(struct.pack('>b', math.floor(battery * 10)))
+message_payload.extend(struct.pack('>H', math.floor(pressure)))
+message_payload.extend(struct.pack('>H', math.floor(light)))
 
 # Start LTE Connection
 lte_wrapper = LTEWrapper(band=IOTCREATORS_BAND, apn=IOTCREATORS_APN)
 lte_wrapper.start_lte_connection()
 
+# Add LTE Connection Time for Benchmarking
+message_payload.extend(struct.pack('>H', math.floor(lte_wrapper.elapsed_connection_time)))
+
 # Send a message to iotcreators backend
 message.send(message_payload)
 
-# Tear down LTE
-lte_wrapper.stop_lte_connection()
-lte_wrapper.deinit()
+# Idle a bit
+time.sleep(2)
 
 # Sleep device
-py.setup_sleep(SLEEP_TIME)
+py.setup_sleep(SLEEP_TIME - stopwatch.read())
 py.go_to_sleep(pycom_module_off=True, accelerometer_off=True, wake_interrupt=False)
